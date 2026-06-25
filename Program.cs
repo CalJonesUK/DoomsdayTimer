b770b6423dcc3ae21012f76e0b78466e33e68d96
CalJonesUK <cjonesy9@googlemail.com>
2026-06-26 00:19:04 +0100

    Initial commit - Doomsday Timer

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Welcome to dooms pin code entry, enter the wrong password and the world will end \n");
+Thread.Sleep(3000); // 1000 milliseconds = 1 second
+Console.WriteLine("Enter the pin code: ");
+
+var pin = "1234"; // The correct pin code
+var userInput = Console.ReadLine();
+
+if (pin == userInput)
+{
+    Console.WriteLine("Pin code accepted, world is saved.");
+}
+else
+{
+    Console.WriteLine("Incorrect pin code. The world will end.");
+
+    Console.WriteLine("5 \n");
+    Thread.Sleep(1000); // 1000 milliseconds = 1 second
+
+    Console.WriteLine("4 \n");
+    Thread.Sleep(1000); // 1000 milliseconds = 1 second
+
+    Console.WriteLine("3 \n");
+    Thread.Sleep(1000); // 1000 milliseconds = 1 second
+
+    Console.WriteLine("2 \n");
+    Thread.Sleep(1000); // 1000 milliseconds = 1 second
+
+    Console.WriteLine("1 \n");
+}
+
+
+
+Console.ReadKey(); //Prevents the console from closing immediately after running the program.gfg
