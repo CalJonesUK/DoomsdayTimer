6ade67203e50cd2e565f64ad97a7014a062508f1
SkepticleOrc <cjonesy9@googlemail.com>
2026-06-27 17:18:07 +0100

    Added new C# files and updated game logic

--- Program.cs
+++ Program.cs
@@ -1,33 +1,74 @@
-﻿Console.WriteLine("Welcome to dooms pin code entry, enter the wrong password and the world will end \n");
-Thread.Sleep(3000); // 1000 milliseconds = 1 second
-Console.WriteLine("Enter the pin code: ");
+﻿Console.WriteLine("PA system: 10 minutes of oxygen remaining. \n"); //As the player is coming to, they hear a PA system announcement warning them of the limited oxygen supply.
+Thread.Sleep(5000);
+Console.Clear();
+Console.WriteLine("You wake up, Trapped inside a capsule. Upon coming to your senses, you realize you are floating in space.\n\nYou look around and see a small LCD control panel in front of you. ");
+Thread.Sleep(10000);
+Console.Clear();
+//--------------------------------------------------------------------------------------------------------------------------------
 
-var pin = "1234"; // The correct pin code
-var userInput = Console.ReadLine();
+Console.WriteLine("Do you investigate the panel? \n ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Y / N: ");
 
-if (pin == userInput)
+string investigate = Console.ReadLine().ToUpper();
+Console.ResetColor();
+Console.Clear();
+
+if (investigate == "Y" || investigate == "YES")
 {
-    Console.WriteLine("Pin code accepted, world is saved.");
+    Console.WriteLine("You investigate panel, here is what you find:\n\nThe dusk settles softly, and starlight begins its jo1urney.\r\nOld tales drift on the wind, carrying se2crets long forgotten.\r\nShadows move with quiet grace, weaving my3steries in the dark.\r\nAnd those who listen closely may uncov4er what others miss.\r\n");
+    Console.ForegroundColor= ConsoleColor.Green;
+    Console.WriteLine("Press Enter when ready...");
+    Console.ReadKey();
+    Console.ResetColor();
+    Console.Clear();
+}
+else if (investigate == "N" || investigate == "NO") //This response would trigger a death screen. Ending the program.
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("██╗   ██╗ ██████╗ ██╗   ██╗    ██████╗ ██╗██████╗ \r\n██║   ██║██╔═══██╗██║   ██║    ██╔══██╗██║██╔══██╗\r\n██║   ██║██║   ██║██║   ██║    ██████╔╝██║██║  ██║\r\n╚██╗ ██╔╝██║   ██║██║   ██║    ██╔══██╗██║██║  ██║\r\n ╚████╔╝ ╚██████╔╝╚██████╔╝    ██████╔╝██║██████╔╝\r\n  ╚═══╝   ╚═════╝  ╚═════╝     ╚═════╝ ╚═╝╚═════╝ "); 
+    Thread.Sleep(2000);
+    Environment.Exit(0); //Used to terminate the program immediately after the player dies.
+}
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+Console.WriteLine("Doomsday Timer: Pin authentication required \n");
+Thread.Sleep(1000); // 1000 milliseconds = 1 second
+Console.Write("Authentication code: "); //Console.Write() is used to keep the cursor on the same line after printing the text.
+
+string pin = "1234"; // The correct pin code
+string userInput = Console.ReadLine();
+
+if (userInput == pin)
+{
+    Console.WriteLine("Authentication accepted: Failsafe deactivated.");
+    Thread.Sleep(5000);
+    Console.Clear();
+    Console.WriteLine("'PA System: Failsafe deactivated. Oxygen supply restored, capsule power restored'\n");
+    Thread.Sleep(5000);
+    Console.Clear();
+    Console.WriteLine("TBC");
+    Thread.Sleep(5000);
+    Environment.Exit(0); //Remove "Environment.Exit(0)" if you carry on the story, this is just a placeholder for now.
 }
 else
 {
-    Console.WriteLine("Incorrect pin code. The world will end.");
+    Console.WriteLine("Authentication failed: Failsafe initiated. \n");
 
-    Console.WriteLine("5 \n");
-    Thread.Sleep(1000); // 1000 milliseconds = 1 second
+    for (int countdown = 5; countdown > 0; countdown--)
+    {
+        Console.WriteLine("Countdown:" + countdown);
+        Thread.Sleep(1000); // 1000 milliseconds = 1 second
+        Console.Clear();
+    }
 
-    Console.WriteLine("4 \n");
-    Thread.Sleep(1000); // 1000 milliseconds = 1 second
-
-    Console.WriteLine("3 \n");
-    Thread.Sleep(1000); // 1000 milliseconds = 1 second
-
-    Console.WriteLine("2 \n");
-    Thread.Sleep(1000); // 1000 milliseconds = 1 second
-
-    Console.WriteLine("1 \n");
+    Console.WriteLine("Failsafe triggered: Earth destruction completed. You have 2 minutes of oxygen remaining.");
+    Thread.Sleep(5000);
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("██╗   ██╗ ██████╗ ██╗   ██╗    ██████╗ ██╗██████╗ \r\n██║   ██║██╔═══██╗██║   ██║    ██╔══██╗██║██╔══██╗\r\n██║   ██║██║   ██║██║   ██║    ██████╔╝██║██║  ██║\r\n╚██╗ ██╔╝██║   ██║██║   ██║    ██╔══██╗██║██║  ██║\r\n ╚████╔╝ ╚██████╔╝╚██████╔╝    ██████╔╝██║██████╔╝\r\n  ╚═══╝   ╚═════╝  ╚═════╝     ╚═════╝ ╚═╝╚═════╝ ");
+    Thread.Sleep(2000);
+    Environment.Exit(0);
 }
-
-
-
-Console.ReadKey(); //Prevents the console from closing immediately after running the program.gfg
